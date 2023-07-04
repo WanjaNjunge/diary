aea3459ebc72fa2991762bc54fc110cd8a84a678
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-04 18:34:23 +0300

    set up necessary  authentication configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(username: "lilian", password: "lilo", age: 25)
+user = User.create(username: "lazarus", password: "mary", age: 25)
+user = User.create(username: "pablo", password: "lobpa", age: 25)
+user = User.create(username: "gustavo", password: "stavo", age: 25)
+user = User.create(username: "gringo", password: "grin", age: 25)
