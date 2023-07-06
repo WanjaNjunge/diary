a6749e3f3a9593194c759c1310b5619947b3b2ac
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-06 08:40:07 +0300

    Setting Up Router, Context and the Reducer

---
+++ client/src/AppState.js
@@ -0,0 +1,41 @@
+
+import React, { useReducer } from "react";
+
+
+//INITIAL STATE
+const initialState = {
+  url: "http://diary-up7p.onrender.com"
+}
+
+// REDUCER
+const reducer = (state, action) => {
+  
+  switch(action.type){
+    default:
+      return state
+  }
+}
+
+
+// APPCONTEXT
+
+const AppContext = React.createContext(null)
+
+
+
+//AppState component
+export const AppState = (props) => {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return <AppContext.Provider value={{state, dispatch}}>
+    {props.children}
+  </AppContext.Provider>
+
+}
+
+
+//useAppState hook
+export const useAppState = () => {
+  return React.useContext(AppContext)
+}
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router } from 'react-router-dom';
+import { AppState } from './AppState';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AppState>
+    <Router>
     <App />
+    </Router>
+    </AppState>
+    
+    
   </React.StrictMode>
 );
 
