0d32792416db35af063f4d35fd6a88bde9e726cc
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-07 04:53:22 +0300

    Add logout functionality to the home page

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,14 +1,38 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useAppState } from "../AppState"
 
 const Nav = (props) => {
+
+    const { state, dispatch } = useAppState();
+
     return <header>
         <h1>My Diary App</h1>
         <nav>
-            <Link to="/"><div>Home</div></Link>
-            <Link to="/auth/signup"><div>Signup</div></Link>
-            <Link to="/auth/login"><div>Login</div></Link>
-        </nav>
+        {!state.token ? (
+          <>
+            <Link to="/">
+              <div>Home</div>
+            </Link>
+            <Link to="/auth/signup">
+              <div>Signup</div>
+            </Link>
+            <Link to="/auth/login">
+              <div>Login</div>
+            </Link>
+          </>
+        ) : null}
+        {state.token ? (
+          <div
+            onClick={() => {
+              dispatch({ type: "logout" });
+              props.history.push("/");
+            }}
+          >
+            Logout
+          </div>
+        ) : null}
+      </nav>
     </header>
 }
 
