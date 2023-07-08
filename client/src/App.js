d3dfd353fc37752d3cdaa5aa6357441a3dd38cb1
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-08 16:00:36 +0300

    Starting to work on crud

--- client/src/App.js
+++ client/src/App.js
@@ -7,27 +7,25 @@ import Dashboard from './pages/Dashboard'
 import { useAppState } from "./AppState"
 
 export const App = (props) => {
-
-
-    const {state, dispatch} = useAppState()
+    const { state, dispatch } = useAppState();
     React.useState(() => {
-        const auth = JSON.parse(window.localStorage.getItem("auth"));
-        if (auth){
-            dispatch({ type: "auth", payload: auth });
-            props.history.push("/dashboard");
-        } else {
-            props.history.push("/");
-        }
-    }, [])
-
-
-    return <>
-    <Nav />
-    <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/auth/:form" component={Auth} />
-        <Route path="/dashboard" component={Dashboard} />
-    </Switch>
-    </>
-};
-
+      const auth = JSON.parse(window.localStorage.getItem("auth"));
+      if (auth) {
+        dispatch({ type: "auth", payload: auth });
+        props.history.push("/dashboard");
+      } else {
+        props.history.push("/");
+      }
+    }, []);
+  
+    return (
+      <>
+        <Route path="/" component={Nav}/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth/:form" component={Auth} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </>
+    );
+  };
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,15 +1,40 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import { useAppState } from "../AppState"
 
 const Nav = (props) => {
-    return <header>
-        <h1>My Diary App</h1>
+    const { state, dispatch } = useAppState();
+  
+    return (
+      <header>
+        <h1>Alex Merced Note Taking App</h1>
         <nav>
-            <Link to="/"><div>Home</div></Link>
-            <Link to="/auth/signup"><div>Signup</div></Link>
-            <Link to="/auth/login"><div>Login</div></Link>
+          {!state.token ? (
+            <>
+              <Link to="/">
+                <div>Home</div>
+              </Link>
+              <Link to="/auth/signup">
+                <div>Signup</div>
+              </Link>
+              <Link to="/auth/login">
+                <div>Login</div>
+              </Link>
+            </>
+          ) : null}
+          {state.token ? (
+            <div
+              onClick={() => {
+                dispatch({ type: "logout" });
+                props.history.push("/");
+              }}
+            >
+              Logout
+            </div>
+          ) : null}
         </nav>
-    </header>
-}
-
-export default Nav
+      </header>
+    );
+  };
+  
+  export default Nav;
