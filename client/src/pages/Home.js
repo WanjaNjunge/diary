1c55cec254809bb05016104283f83bca419620a4
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-09 23:39:00 +0300

    Redirect home page button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = (props) => {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center vh-100">
       <h1 className="display-4">Welcome to the Notes App</h1>
       <p className="lead">Take notes and stay organized</p>
-      <button className="bn53">Get Started</button>
+      <Link to="/auth/signup" className="bn53">Get Started</Link>
     </div>
   );
 };
