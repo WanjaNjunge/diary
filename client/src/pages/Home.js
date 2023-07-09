d0e45249a091fe6b9fd30cdd74a2fa6ad0ac881a
wanja <114565394+WanjaNjunge@users.noreply.github.com>
2023-07-09 22:57:30 +0300

    Add styling to the home component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,13 @@
-import React from "react"
+import React from "react";
 
 const Home = (props) => {
-    return <h1>Home</h1>
-}
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <h1 className="display-4">Welcome to the Notes App</h1>
+      <p className="lead">Take notes and stay organized</p>
+      <button className="btn btn-primary btn-lg">Get Started</button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
